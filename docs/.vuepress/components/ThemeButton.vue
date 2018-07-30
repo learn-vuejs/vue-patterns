9e7464bfa5b39a7460099a0e5bb9b51084bd0bcd
ilkwon sim <simsim0709@gmail.com>
2018-07-30 21:50:13 +0900

    Add provide/inject working example

---
+++ docs/.vuepress/components/ThemeButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="demo">
+    <button class="btn" :style="{ color: '#fff', backgroundColor: primary && theme.primaryColor }">
+      <slot></slot>
+    </button>
+  </p>
+</template>
+
+<script>
+export default {
+  inject: {
+    theme: {
+      default: {
+        primaryColor: 'darkseagreen',
+      },
+    },
+  },
+  props: {
+    primary: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
---
+++ docs/.vuepress/components/ThemeProvider.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  provide: {
+    theme: {
+      primaryColor: '#3eaf7c',
+    },
+  },
+  render(h) {
+    return this.$slots.default[0];
+  },
+};
+</script>

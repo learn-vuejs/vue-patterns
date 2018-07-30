d740f924162921d7ce00046f8ea9fdeb16102209
ilkwon sim <simsim0709@gmail.com>
2018-07-30 22:09:18 +0900

    Add Passing Props & Listners working example

---
+++ docs/.vuepress/components/PassingProps.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <passing-props-child v-bind="$attrs" v-on="$listeners"></passing-props-child>
+  </div>
+</template>
+
+<script>
+import PassingPropsChild from './PassingPropsChild';
+
+export default {
+  components: {
+    PassingPropsChild,
+  },
+  inheritAttrs: false,
+  props: {
+    title: {
+      type: String,
+      default: 'Hello, Vue!',
+    },
+  },
+};
+</script>

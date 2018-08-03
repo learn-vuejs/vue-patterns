52c20a6ca4d95b146b7ed41828f0c1e6c03fef9a
ilkwon sim <simsim0709@gmail.com>
2018-08-04 00:40:51 +0900

    Add head and icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,121 @@ module.exports = {
   description:
     'Useful Vue patterns, techniques, tips and tricks and curated helpful links.',
   serviceWorker: true,
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/icons/apple-icon-57x57.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/icons/apple-icon-60x60.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/icons/apple-icon-72x72.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/icons/apple-icon-76x76.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/icons/apple-icon-114x114.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/icons/apple-icon-120x120.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/icons/apple-icon-144x144.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/icons/apple-icon-152x152.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/icons/apple-icon-180x180.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/icons/android-icon-192x192.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/icons/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/icons/favicon-96x96.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/icons/favicon-16x16.png',
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    [
+      'script',
+      { defer: true, async: true, src: 'https://buttons.github.io/buttons.js' },
+    ],
+  ],
   themeConfig: {
     displayAllHeaders: true,
     lastUpdated: 'Last Updated',

ff01ac3e7a3dbf4ecaa1f5b70692b1f5ce71b8fd
Ilkwon Sim (from Travis CI) <simsim0709@gmail.com>
2018-08-21 14:36:14 +0000

    Deploy learn-vuejs/vue-patterns to github.com/learn-vuejs/vue-patterns.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "991dc6d676b557ef57549a11ccb3f570"
+    "revision": "0e0613481bc1d7dbe0a3341589b0df2f"
   },
   {
     "url": "assets/css/0.styles.a3e46cac.css",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "revision": "3ca0423cab3972e089e126b06f89a050"
   },
   {
-    "url": "assets/js/17.82485f66.js",
-    "revision": "be6b4d7af09f571fad044049d68e205b"
+    "url": "assets/js/17.0262594c.js",
+    "revision": "b97ca95e677a9feccd3823198e430c3c"
   },
   {
     "url": "assets/js/2.178aa6c2.js",
@@ -80,12 +80,12 @@ self.__precacheManifest = [
     "revision": "1832c215430ee8cf030c22afad160aaa"
   },
   {
-    "url": "assets/js/6.3ed7faf0.js",
-    "revision": "3fd78fdcbd68f8ee7bdf4f6716aefdac"
+    "url": "assets/js/6.7d9b504a.js",
+    "revision": "8e8d761b74670862784186e756a6a370"
   },
   {
-    "url": "assets/js/7.6eb5d3b4.js",
-    "revision": "e78f7d642192aa492d8caa8677ebdf85"
+    "url": "assets/js/7.f5a3d41b.js",
+    "revision": "118574825d28d0bafc3109e266500e0b"
   },
   {
     "url": "assets/js/8.0e1f05c7.js",
@@ -96,8 +96,8 @@ self.__precacheManifest = [
     "revision": "f28a38c977363cf35e4c20e652db9997"
   },
   {
-    "url": "assets/js/app.073ad3f4.js",
-    "revision": "8dbe8e8946498686d530defae0d38d79"
+    "url": "assets/js/app.b6407148.js",
+    "revision": "60865d81145217fd43b770bb234d8939"
   },
   {
     "url": "icons/android-icon-144x144.png",
@@ -197,7 +197,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "4391cae78619a0604279fa3257b5e96f"
+    "revision": "82df1e61909374dd6f2ae6fc9a2b139e"
   },
   {
     "url": "learn-vue-logo.png",
@@ -205,19 +205,19 @@ self.__precacheManifest = [
   },
   {
     "url": "patterns/index.html",
-    "revision": "81e5cfa33d57f2a769ff864c8e7f3b9a"
+    "revision": "c070546e203d1e22496bbff350134f20"
   },
   {
     "url": "sponsors/index.html",
-    "revision": "4b2bad286a67555becafd18542abcd4f"
+    "revision": "7689cf3085f20c2e44fa7cc91a02f0cc"
   },
   {
     "url": "translations/index.html",
-    "revision": "547bba3b40b194b349d7de3e480542dc"
+    "revision": "c387aff41e19604889e12886ac19819b"
   },
   {
     "url": "useful-links/index.html",
-    "revision": "442173df2dab4a0adf615627e7f3762d"
+    "revision": "036a03557df8813d03aee5eaf7bdd3d2"
   },
   {
     "url": "vue-patterns-hero.png",
